739a87bf95a703c29ccc5788d00df2f96f0a5e0f
hoonhur <66529909+hoonhur@users.noreply.github.com>
2020-07-21 00:52:39 -0400

    Add files via upload

--- src/server/server.js
+++ src/server/server.js
@@ -1,35 +1,21 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = [];
+projectData = []
 
-// Require Express to run server and routes
-const express = require('express');
+const express = require('express')
+const app = express()
+app.use(express.static('dist'))
 
-// Start up an instance of app
-const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
+const cors = require('cors')
+app.use(cors())
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const regeneratorRuntime = require('regenerator-runtime')
 
-// Cors for cross origin allowance
-const cors = require('cors');
-app.use(cors());
-
-// Initialize the main project folder
-app.use(express.static('website'));
-
-// Setup Server
-const port = 8000;
-
-// Spin up the server
-// Callback to debug
-const server = app.listen(port, () => console.log(`running on localhost: ${port}`));
-
-// Initialize all route with a callback function
-// Callback function to complete GET '/all'
+app.get('/', (req, res) => res.sendFile('dist/index.html'))
+app.listen(8000, () => console.log(`running on localhost: 8000`));
 
 app.get('/all', (req,res) => {
     res.send(projectData);
