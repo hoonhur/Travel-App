2ed45870b19bd95acbf8944c0f662f9ce597a53e
Hoon Hur <hoonhur@outlook.com>
2020-07-23 09:19:20 -0400

    3rd draft
    Add function to change date format and postData function

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,24 +1,27 @@
 // Decalaration of Global Variables
 
 
+// Function
+
 /* Check Input Date
 1. check format
 2. check difference number of date between today*/
 
-// Function
+// Function change date format to get API
 
-// Async Func for Geonames
-// Post Route for Geonames API (/api)
+// Async Functikn for Geonames
+// Call Function postData for Geonames API (/api)
 
 // Async Func for Weatherbit
 // Forecast 16 days for depart within 16 days
-// Historical weather for date diff is over 16 days (including past date)
+// Historical weather for date difference is over 16 days (including past date)
 
-// Post Route for Weatherbit API (/api)
+// Call Function postData for Weatherbit API (/api)
 
 // Async Func for pixabay
-// Post Route for Pixabay API (/api)
+// Call Function postData for Pixabay API (/api)
 
+// POST Route, function postData
 // GET Route for Project Data (/projectdata)
 // Update UI with Project Data
 
