fce51956db8d6467b44c0379324bfd26dc05a43c
Hoon Hur <hoonhur@outlook.com>
2020-07-23 08:16:44 -0400

    1st draft structure
    1st draft structure with comments

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,1 +1,15 @@
+// Decalaration of Global Variables
+
+
+// Function
+
+// Async Func for Geonames
+// Async Func for Weatherbit
+// Async Func for pixabay
+
+// GET Route for Project Data
+// Update UI with Project Data
+
+
+
 
