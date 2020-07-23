2e7e31f7b4fd04a1ddd95bfd8c5e6071700788dc
Hoon Hur <hoonhur@outlook.com>
2020-07-23 09:22:08 -0400

    Correct typo

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -7,22 +7,24 @@
 1. check format
 2. check difference number of date between today*/
 
-// Function change date format to get API
+// Function to change date format to get API
 
-// Async Functikn for Geonames
-// Call Function postData for Geonames API (/api)
+// Async Function for Geonames
+// Call Function postData for Geonames API
 
 // Async Func for Weatherbit
-// Forecast 16 days for depart within 16 days
-// Historical weather for date difference is over 16 days (including past date)
+// Forecast 16 days API for input date within 16 days
+// Historical weather API for date difference is over 16 days (including past date)
 
-// Call Function postData for Weatherbit API (/api)
+// Call Function postData for Weatherbit API
 
 // Async Func for pixabay
-// Call Function postData for Pixabay API (/api)
+// Call Function postData for Pixabay API
 
 // POST Route, function postData
+
 // GET Route for Project Data (/projectdata)
+
 // Update UI with Project Data
 
 
