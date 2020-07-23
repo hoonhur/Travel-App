d102abc8436024545a67fde0876eb75fec64cad2
Hoon Hur <hoonhur@outlook.com>
2020-07-23 14:42:29 -0400

    Pull postData function from my Weather Journal App
    Pull postData function

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -52,6 +52,23 @@ getGeonames(geoBaseUrl, city, geoUsername)
 // Call Function postData for Pixabay API
 
 // POST Route, function postData
+const postData = async(url = '', data = {}) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    try {
+        const newData = await res.json();
+        console.log(newData);
+        return newData;
+    }catch(error) {
+        console.log("error", error);
+    }
+};
 
 // GET Route for Project Data (/projectdata)
 
