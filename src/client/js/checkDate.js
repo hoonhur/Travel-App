844e63dcc7c91ce384b2a8ebdf3df6c041ed8405
Hoon Hur <hoonhur@outlook.com>
2020-07-22 21:22:49 -0400

    Update and rename checkInput.js to checkDate.js
    finish checkDate function to validate input date

---
+++ src/client/js/checkDate.js
@@ -0,0 +1,13 @@
+function checkDate (date) {
+        // regular expression to match required date format
+    const regexp = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+    if(date != '' && !date.match(regexp)) {
+        alert("Invalid date format. please input date as 'mm/dd/yyyy' format");
+        return false;
+    } else {
+        console.log('Date input is valid')
+        return true;
+    }
+}
+
+export { checkDate }
