2e8427b901f6d2521ce14efcdb4bc4a186721efa
Hoon Hur <hoonhur@outlook.com>
2020-07-23 08:19:55 -0400

    2nd draft structure
    Add POST Route for each API

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -4,12 +4,13 @@
 // Function
 
 // Async Func for Geonames
+// Post Route for Geonames API (/api)
+
 // Async Func for Weatherbit
+// Post Route for Weatherbit API (/api)
+
 // Async Func for pixabay
+// Post Route for Pixabay API (/api)
 
-// GET Route for Project Data
+// GET Route for Project Data (/projectdata)
 // Update UI with Project Data
-
-
-
-
