a934441f6e7f9e9138ceae35853552c1fbf2692d
Hoon Hur <hoonhur@outlook.com>
2020-07-23 08:25:43 -0400

    Add check input date
    App will check input date to see it is within 16 days from today. If it is within 16 days from today, APP will collect forecast weather; else APP pull historical weather on the same date.

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,12 +1,19 @@
 // Decalaration of Global Variables
 
 
+/* Check Input Date
+1. check format
+2. check difference number of date between today
+
 // Function
 
 // Async Func for Geonames
 // Post Route for Geonames API (/api)
 
 // Async Func for Weatherbit
+// Forecast 16 days for depart within 16 days
+// Historical weather for date diff is over 16 days (including past date)
+
 // Post Route for Weatherbit API (/api)
 
 // Async Func for pixabay
@@ -14,3 +21,7 @@
 
 // GET Route for Project Data (/projectdata)
 // Update UI with Project Data
+
+
+
+
