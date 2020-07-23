f91f564b268d1861b5e72c8b2d3c5de159c6c0e0
Hoon Hur <hoonhur@outlook.com>
2020-07-23 11:14:23 -0400

    Add main function and getGeonames
    Add only function structure

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -7,17 +7,30 @@ const geoUsername = '&username=hunhuh10'
 
 
 // Function
+function handleSubmit () {
+//declaration of variables
+let city
+let date
+let geoDate
 
 /* Check Input Date
 1. check format
 2. check difference number of date between today*/
+Client.checkDate(date);
+checkDiffDays(date);
 
-// Function to change date format to get API
+// Function to change date format to get API for over 16 days
+if(diffDays < 0 & diffDays >= 16) {
+changeDateFormat(date)
+} else {
+geoDate = date
+};
 
 // Async Function for Geonames
-
+getGeonames(city)
 // Call Function postData for Geonames API
-
+.then (function(geoData){
+}) 
 // Async Func for Weatherbit
 // Forecast 16 days API for input date within 16 days
 // Historical weather API for date difference is over 16 days (including past date)
