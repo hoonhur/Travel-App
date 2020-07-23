f3bbc901392adc870c74bb6b758b66e187c063a8
Hoon Hur <hoonhur@outlook.com>
2020-07-23 14:34:20 -0400

    Add changeDateFormat func, Declare G-var
    Add functions and parameters for geonames.

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -7,11 +7,17 @@ const geoUsername = '&username=hunhuh10'
 
 
 // Function
+
+//changeDateFormat(d) {
+  geoDate = d[6] + d[7] + d[8] + d [9] + '-' + d[0] + d[1] + '-' + d[3] + d[4];
+  return geoDate;
+}
+
 function handleSubmit () {
 //declaration of variables
-let city
-let date
-let geoDate
+let city = document.getElementById(city).value;
+let date = document.getElementById(date).value;
+let geoDate = date;
 
 /* Check Input Date
 1. check format
@@ -21,16 +27,20 @@ checkDiffDays(date);
 
 // Function to change date format to get API for over 16 days
 if(diffDays < 0 & diffDays >= 16) {
-changeDateFormat(date)
-} else {
-geoDate = date
+  changeDateFormat(date)
 };
 
 // Async Function for Geonames
-getGeonames(city)
+getGeonames(geoBaseUrl, city, geoUsername)
 // Call Function postData for Geonames API
 .then (function(geoData){
-}) 
+  postData('/addData', {
+    country = geoData.country
+    city = geoData.city
+    latitude = goeData.lat
+    longitude = geoData.lng
+})
+
 // Async Func for Weatherbit
 // Forecast 16 days API for input date within 16 days
 // Historical weather API for date difference is over 16 days (including past date)
