175c4ba109014aab85b79e8254c7ef62ae33bd50
Hoon Hur <hoonhur@outlook.com>
2020-07-23 02:31:00 -0400

    resolve error for checkDate function
    import and export checkDate function in index.js to access it in applicaion.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,10 @@
 import { handleSubmit } from './js/application'
+import { checkDate } from './js/checkdate'
 
 document.getElementById('generate').addEventListener('click', handleSubmit)
 
 export { handleSubmit}
+export { checkDate}
 
 import './styles/base.scss'
 import './styles/button.scss'
