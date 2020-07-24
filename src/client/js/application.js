6b24b945b3a46d89f85c58ab637cfaa45506102c
Hoon Hur <hoonhur@outlook.com>
2020-07-24 09:11:11 -0400

    Need to solve wherever API issue

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,8 +1,20 @@
 // Decalaration of Global Variables
-let tripData = {}
+// URL for geonames
+const geoBaseUrl = 'http://api.geonames.org/postalCodeSearchJSON?placename='
+const geoUsername = '&username=hunhuh10'
+
+// URL for weatherbit
+const wthrBaseUrl = 'https://api.weatherbit.io/v2.0/forecast/daily?city='
+const wthrHstrUrl = 'https://api.weatherbit.io/v2.0/history/daily?city='
+const wthrKey = '&key=0354bba909aa42b0a47bd1f252e02b21'
+
+// URL for pixabay
+const pixBaseUrl = 'https://pixabay.com/api/?key=17552769-fee70d93d21f168f4a1a5c00a&image_type=photo&category=travel&q'
+
 let diffDays
 
 // Function //
+
 // Function to check number of difference of days from today
 function checkDiffDays(d) {
     let today = new Date()
@@ -22,50 +34,112 @@ function changeDateFormat(d) {
     return whtrDate;
 }
 
-// Main Function
 function handleSubmit() {
-    //declaration of variables
+//declaration of variables
     let city = document.getElementById('city').value;
     let date = document.getElementById('date').value;
     let whtrDate = date;
   
-    // Check Input Date
-    // 1. check format
+// Check Input Date
+// 1. check format
     Client.checkDate(date);
-    // 2. check difference number of date between today*/
+// 2. check difference number of date between today*/
     checkDiffDays(date);
-    // Function to change date format to get API for over 16 days
+// Function to change date format to get API for over 16 days
     if(diffDays < 0 && diffDays >= 16) {
         changeDateFormat(date)
     };
-    // Call Function to get Geonames API
-    postData('http://localhost:8082/geonames', {city: city})
-    //
-    .then(() => {
-        if(diffDays >= 0 && diffDays < 16) {
-            postData('http://localhost:8082/weatherFcst', {
-                city: city,
-                country: tripData.countryCode
-                })
-        } else {
-            postData('http://localhost:8082/weatherHstr', {
-                city: city,
-                country: tripData.countryCode,
-                date: whtrDate
-                })
-        }
-        })
-    // Call Function to get pixabay API
-    .then(() => {
-        postData('http://localhost:8082/pixabay', {
-            city: city,
-            country: tripData.countryCode
+// Call Function to get Geonames API
+    getGeonames(geoBaseUrl, city, geoUsername)
+// Call Function postData for Geonames API
+    .then(function(geoData) {
+        postData('/addData', {
+            country: geoData.countryCode,
+            city: geoData.placeName,
+            latitude: geoData.lat,
+            longitude: geoData.lng
         })
     })
-
+// Call Function to get Weatherbit API and postData
+// Forecast 16 days weather API for input date within 16 days
+// Historical weather API for date difference is over 16 days (including past date)
+    .then(() => {
+        if(diffDays >= 0 && diffDays < 16) {
+            getWthrFcst(wthrBaseUrl, city, geoData.countryCode, wthrKey)
+            .then(function(wthrData) {
+                postData('/addData', {
+                    highTemp: wthrData.data[diffDays].max_temp,
+                    lowTemp: wthrData.data[diffDays].min_temp,
+                    description: wthrData.data[diffDays].weather.description
+                })
+            })
+        } else {
+        getWthrHstr(wthrHstrUrl, city, geoData.countryCode, whtrDate, wthrKey)
+        .then(function(wthrData) {
+            postData('/addData', {
+                highTemp: wthrData.data.max_temp,
+                lowTemp: wthrData.data.min_temp,
+                description: 'This is historical weather.'
+            })
+        })
+        }
+    })
+// Call Function to get pixabay API
+    .then(getPixabay(pixBaseUrl, city, geoData.countryCode))
+// Call Function postData for Pixabay API
+    .then(function(){
+        postData ()
+    })
 // Call Function updateUI
     .then(function(){updateUI()}
     )
+}
+
+// Function to get geonames API
+const getGeonames = async (url, city, username) => {
+    const req = await fetch (url+city+username)
+    try {
+        const geoRawData = await req.json()
+        const geoData = geoRawData.postalCodes[0]
+        console.log(geoData)
+        return geoData
+    } catch (error) {
+        console.log('Error at getGeonames', error)
+    }
+}
+
+//Function to get weatherbit API
+const getWthrFcst = async (url, city, country, key) => {
+    const req = await fetch(url+city+','+country+key)
+    try {
+        wthrData = await req.json()
+        console.log(wthrData)
+        return wthrData
+    } catch(error) {
+        console.log('Error at getWthrFcst', error)
+    }
+}
+const getWthrHstr = async (url, city, country, date, key) => {
+    const req = await fetch(url+city+','+country+'&start_date'+date+'&end_date'+date+key)
+    try {
+        wthrData = await req.json()
+        console.log(wthrData)
+        return wthrData
+    } catch(error) {
+        console.log('Error at getWthrHstr', error)
+    }
+}
+
+// Function to get pixabay API
+const getPixabay = async (url, city, country) => {
+    const req = await fetch(url+city+','+country)
+    try {
+        geoData = await req.json()
+        console.log(geoData)
+        return geoData
+    } catch(error) {
+        console.log('Error at getPixabay', error)
+    }
 }
 
 // POST Route, function postData
@@ -81,25 +155,15 @@ const postData = async(url = '', data = {}) => {
     try {
         const newData = await res.json();
         console.log(newData);
-        tripData = newData
-    } catch(error) {
+        return newData;
+    }catch(error) {
         console.log("error", error);
     }
 };
 
-const getData = async (url) => {
-    const res = await fetch ('http://localhost:8082/projectData')
-    try {
-        const data = await res.json()
-        return data
-    } catch(error) {
-        console.log('error at getData', error)
-    }
-}
-
 // GET Route for Project Data and updateUI
 const updateUI = async() =>{
-    const request = await fetch('http://localhost:8082/projectData');
+    const request = await fetch('/all');
     try{
         const allData = await request.json()
         const last = allData.length - 1;
