304b63fe75bfc0d3201535b55510e5081cde1fab
Hoon Hur <hoonhur@outlook.com>
2020-07-26 08:15:26 -0400

    Add two new tests
    All tests work well

---
+++ __Test__/cleartirpsubmit.test.js
@@ -0,0 +1,7 @@
+import { cleartripSubmit } from '../src/client/js/application'
+
+describe('Testing cleartripSubmit functionality', () => {
+    test('cleartripSubmit should be defined', () => {
+        expect(cleartripSubmit).toBeDefined()
+    })
+})
--- __Test__/server.test.js
+++ __Test__/server.test.js
@@ -1,10 +1,11 @@
 const app = require('../src/server/server')
 
+const supertest = require('supertest')
+const request = supertest(app)
+
 describe('Testing Expresss Server POST ROUTE functionality', () => {
     test('APP should send projectData to user', async () => {
-        const res = await request(app)
-        .post('/addData')
-        .send(projectData)
-    expect(res.statusCode).toEqual(200)
+        const res = await request.get('/getData')
+        expect(res.statusCode).toBe(200)
     })
 })
